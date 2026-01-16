55a846489f462506f6c0cb854d99b5d4910bbaaa
Hedi Ben Khalifa <hedi.benkhalifa@student.vinci.be>
2026-01-16 17:01:49 +0100

    fix: name test in french to english for better comprehension and fix code for dot in last caracter

--- email.test.ts
+++ email.test.ts
@@ -2,29 +2,29 @@ import { validateEmail } from './email';
 
 describe('Email Validation Logic', () => {
 
-    test('devrait refuser s\'il y a un espace', () => {
-        expect(validateEmail('te st@vinci.be')).toBe(false);
+    test('should reject if there is a space', () => {
+        expect(validateEmail('hedi .benkhalifa@student.vinci.be')).toBe(false);
     });
 
-    test('devrait refuser si pas de @', () => {
-        expect(validateEmail('testvinci.be')).toBe(false);
+    test('should reject if there is no @ symbol', () => {
+        expect(validateEmail('hedi.benkhalifa.student.vinci.be')).toBe(false);
     });
 
-    test('devrait refuser si rien avant ou après le @', () => {
-        expect(validateEmail('@vinci.be')).toBe(false);
-        expect(validateEmail('test@')).toBe(false);
+    test('should reject if there is no text before or after the @ symbol', () => {
+        expect(validateEmail('@student.vinci.be')).toBe(false);
+        expect(validateEmail('hedi.benkhalifa@')).toBe(false);
     });
 
-    test('devrait refuser si pas de point dans le domaine', () => {
-        expect(validateEmail('test@vinci')).toBe(false);
+    test('should reject if there is no dot in the domain name', () => {
+        expect(validateEmail('hedi.benkhalifa@studentvinci')).toBe(false);
     });
 
-    test('devrait refuser si le point est le dernier caractère', () => {
-        expect(validateEmail('test@vinci.')).toBe(false);
+    test('should reject if the dot is the last character', () => {
+        expect(validateEmail('hedi.benkhalifa@student.vinci.')).toBe(false);
     });
 
-    test('devrait accepter un email valide', () => {
-        expect(validateEmail('test@vinci.be')).toBe(true);
+    test('should accept a valid email', () => {
+        expect(validateEmail('hedi.benkhalifa@student.vinci.be')).toBe(true);
     });
 
 });
